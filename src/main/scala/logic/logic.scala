9e68e688af79599e3f6a0201fea6c5f9a434b99c
Prescott Murphy <prurph@gmail.com>
2018-02-04 08:59:11 -0500

    Write initial and update methods

--- src/main/scala/logic/logic.scala
+++ src/main/scala/logic/logic.scala
@@ -20,4 +20,29 @@ final case class WorldView(
     time: ZonedDateTime
 )
 
-final class DynAgents[F[_]](implicit M: Monad[F], d: Drone[F], m: Machines[F])
+final class DynAgents[F[_]](implicit M: Monad[F], d: Drone[F], m: Machines[F]) {
+  def initial: F[WorldView] =
+    for {
+      db <- d.getBacklog
+      da <- d.getAgents
+      mm <- m.getManaged
+      ma <- m.getAlive
+      mt <- m.getTime
+    } yield WorldView(db, da, mm, ma, Map.empty, mt)
+
+  def update(old: WorldView): F[WorldView] =
+    for {
+      snap <- initial
+      changed = symdiff(old.alive.keySet, snap.alive.keySet)
+      pending = (old.pending -- changed).filterNot {
+        case (_, started) => timediff(started, snap.time) >= 10.minutes
+      }
+      update = snap.copy(pending = pending)
+    } yield update
+
+  private def symdiff[T](a: Set[T], b: Set[T]) =
+    (a union b) -- (a intersect b)
+
+  private def timediff(from: ZonedDateTime, to: ZonedDateTime): FiniteDuration =
+    ChronoUnit.MINUTES.between(from, to).minutes
+}
